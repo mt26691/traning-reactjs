f1efd23fb6bb59a251282979590d2605403d7585
Manh Tung <nguyenmanhtung848@gmail.com>
2017-06-06 16:38:14 +0700

    react component

---
+++ src/MainContent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					Main Conttent
+    		</div>
+				<hr />
+
+				<footer>
+					<p>&copy; 2016 Manh Tung, Inc.</p>
+				</footer>
+			</div>
+		);
+	}
+}
+
+export default MainContent;
